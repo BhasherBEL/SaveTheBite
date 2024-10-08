6b4ac15af63f6eb0951ef7dfc0483f2921390d7d
Brieuc Dubois <git@bhasher.com>
2024-10-08 16:38:31 +0300

    Initial svelte+tailwind commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>SaveTheBite</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<section></section>
+
+<style>
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<header>
+	<h1 class="text-3xl text-red-500 font-bold m-2">SaveTheBite</h1>
+</header>
+
+<style>
+</style>
