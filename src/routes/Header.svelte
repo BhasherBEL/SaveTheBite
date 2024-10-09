f18952b79b6bc141d1cc08f151848f176143ef15
Brieuc Dubois <git@bhasher.com>
2024-10-09 10:39:21 +0300

    Fix header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,34 +3,32 @@
 
 <header class="bg-white shadow-md py-4 px-8 flex justify-between items-center">
 	<div class="flex items-center space-x-4">
-		<img src="/images/logo.jpg" alt="SaveTheBite Logo" class="h-12" />
+		<a href="/">
+			<img src="/images/logo.jpg" alt="SaveTheBite Logo" class="h-12" />
+		</a>
 	</div>
 	<nav class="flex space-x-8">
 		<div class="flex flex-col items-center group">
 			<!-- Wrap icon and text in a parent div -->
-			<div class="flex flex-col items-center text-gray-800 hover:text-green-500">
+			<div class="flex flex-col items-center text-gray-800 hover:text-primary hover:cursor-pointer">
 				<i class="fas fa-home text-2xl"></i>
 				<span class="text-sm">Home</span>
 			</div>
 		</div>
 		<div class="flex flex-col items-center group">
-			<div class="flex flex-col items-center text-gray-800 hover:text-green-500">
-				<i class="fas fa-info-circle text-2xl"></i>
-				<span class="text-sm">About</span>
+			<div class="flex flex-col items-center text-gray-800 hover:text-primary hover:cursor-pointer">
+				<i class="fas fa-user text-2xl"></i>
+				<span class="text-sm">Account</span>
 			</div>
 		</div>
 		<div class="flex flex-col items-center group">
-			<div class="flex flex-col items-center text-gray-800 hover:text-green-500">
-				<i class="fas fa-user text-2xl"></i>
-				<span class="text-sm">Account</span>
+			<div class="flex flex-col items-center text-gray-800 hover:text-primary hover:cursor-pointer">
+				<i class="fas fa-info-circle text-2xl"></i>
+				<span class="text-sm">About</span>
 			</div>
 		</div>
 	</nav>
 </header>
 
-
-
-
 <style>
 </style>
-
