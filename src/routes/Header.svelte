26cbe1270ebe06cc5123185ba9b8c76ca9a8f765
MxDx <maxime.dxl2002@gmail.com>
2024-11-15 16:36:10 +0200

    header link correction

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
-	export let data;
+	export let data: { user: { username: string } } | null = null;
+
+    function navigateTo(path: string) {
+        window.location.href = path
+    }
+
+    function navigateToAccount() {
+        if (data?.user) {
+            window.location.href = '/account'
+        } else {
+            window.location.href = '/login'
+        }
+    }
 </script>
 
 <header class="bg-white py-4 px-8 flex justify-between items-center">
@@ -12,24 +24,32 @@
 		<div class="flex flex-col items-center group">
 			<!-- Wrap icon and text in a parent div -->
 			<div class="flex flex-col items-center text-gray-800 hover:text-primary hover:cursor-pointer">
-				<i class="fas fa-home text-2xl"></i>
-				<a href="/" class="text-sm">Home</a>
+				<i class="fas fa-home text-2xl" on:click={() => navigateTo('/')}></i>
+				<a href="/" class="text-sm">
+                    <p>Home</p>
+                </a>
 			</div>
 		</div>
 		<div class="flex flex-col items-center group">
 			<div class="flex flex-col items-center text-gray-800 hover:text-primary hover:cursor-pointer">
-				<i class="fas fa-user text-2xl"></i>
+				<i class="fas fa-user text-2xl" on:click={() => navigateToAccount()}></i>
 				{#if data?.user}
-					<a href="/account" class="text-sm">{data?.user.username}</a>
+					<a href="/account" class="text-sm">
+                        <p>{data?.user.username}</p>
+                    </a>
 				{:else}
-					<a href="/login" class="text-sm">Log in</a>
+					<a href="/login" class="text-sm">
+                        <p>Login</p>
+                    </a>
 				{/if}
 			</div>
 		</div>
 		<div class="flex flex-col items-center group">
 			<div class="flex flex-col items-center text-gray-800 hover:text-primary hover:cursor-pointer">
-				<i class="fas fa-info-circle text-2xl"></i>
-				<a href="/about" class="text-sm">About</a>
+				<i class="fas fa-info-circle text-2xl" on:click={() => navigateTo('/about')}></i>
+				<a href="/about" class="text-sm">
+                    <p>About</p>
+                </a>
 			</div>
 		</div>
 	</nav>
