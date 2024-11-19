db6f1503ca5a144a587ef7e4cdcc860fb0f9db46
Brieuc Dubois <git@bhasher.com>
2024-11-19 22:51:29 +0200

    Filter companies

--- src/lib/server/db/vendors.ts
+++ src/lib/server/db/vendors.ts
@@ -2,7 +2,7 @@ import { eq } from 'drizzle-orm';
 import { db } from './client';
 import { vendors, type Vendor } from './schema';
 
-export async function getVendors(): Promise<Vendor[]> {
+export async function getVendors() {
 	const rawVendors = await db.query.vendors.findMany({
 		with: {
 			baskets: {
@@ -19,7 +19,7 @@ export async function getVendors(): Promise<Vendor[]> {
 	return rawVendors;
 }
 
-export async function getVendor(id: number): Promise<Vendor | undefined> {
+export async function getVendor(id: number) {
 	const rawVendors = db.query.vendors.findFirst({
 		where: eq(vendors.id, id),
 		with: {
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -17,25 +17,39 @@ export async function load({ url }: LoadEvent) {
 		distance = 5;
 	}
 
+	let vendors;
+	let longitude: number | undefined;
+	let latitude: number | undefined;
+
 	try {
-		if (!search) return { vendors: await getVendors() };
-
-		const { longitude, latitude } = await addressToCoordinates(search);
-
-		const vendors = await db.query.vendors.findMany({
-			with: {
-				baskets: { with: { sales: true } }
-			}
-		});
-
-		const closeByVendors = vendors.filter(
-			(vendor) =>
-				getDistanceFromLatLonInKm(vendor.latitude, vendor.longitude, latitude, longitude) <=
-				distance
-		);
-		return { vendors: closeByVendors, longitude, latitude };
+		vendors = await getVendors();
 	} catch (error) {
 		console.log('Error fetching vendors:', error);
 		return { vendors: [] };
 	}
+
+	let filteredVendors = [];
+
+	for (const vendor of vendors) {
+		vendor.baskets = vendor.baskets.filter((basket) => basket.sales.length > 0);
+		if (vendor.baskets.length === 0) continue;
+		filteredVendors.push(vendor);
+	}
+
+	vendors = filteredVendors;
+
+	if (search) {
+		try {
+			({ longitude, latitude } = await addressToCoordinates(search));
+			if (longitude && latitude) {
+				vendors = vendors.filter(
+					(vendor) =>
+						getDistanceFromLatLonInKm(vendor.latitude, vendor.longitude, latitude!, longitude!) <=
+						distance
+				);
+			}
+		} catch (e) {}
+	}
+
+	return { vendors: vendors, longitude, latitude };
 }
